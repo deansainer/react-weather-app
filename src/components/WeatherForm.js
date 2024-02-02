3d82d62adb2449be0bf4ae3fd21b974d2e5103fb
deansainer <determind2k@gmail.com>
2024-02-02 01:15:08 +0100

    implemented function to get weather of current city by ip

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -3,11 +3,19 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const WeatherForm = ({setData}) => {
-    const api_key = '86104ffb6aaa5ce42e36c1c32a57ddf0'
-
-    const [city, setCity] = useState(null)
   
+    const [city, setCity] = useState(null)
     
+    async function getWeatherById(e) {
+      try {
+        const response = await axios.get(`https://ipinfo.io/213.134.188.143?token=${process.env.REACT_APP_IP_TOKEN}`)
+        setCity(response.data.city)
+        getWeatherData(e)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
     function handleCityName(event){
         setCity(event.target.value)
     }
@@ -16,9 +24,8 @@ const WeatherForm = ({setData}) => {
         e.preventDefault()
         if (city){
         try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_TOKEN}&units=metric`);
         const weatherData = response.data
-          console.log(weatherData);
         const cityObject = {
             'city': weatherData.name,
             'country': weatherData.sys.country,
@@ -43,6 +50,7 @@ const WeatherForm = ({setData}) => {
     <form className='weather_form' onSubmit={getWeatherData}>
       <input onChange={handleCityName} className="city_input" placeholder="Warsaw"></input>
       <button onClick={getWeatherData} className="btn btn-primary weather_form_btn">Sumbit</button>
+      <img type='submit' alt="geo_btn" src='https://cdn-icons-png.flaticon.com/128/7235/7235992.png' onClick={getWeatherById} className="geo_button"/>
     </form>
     </div>
   );
